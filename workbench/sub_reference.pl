89d1204cd8b7a46726fb7dbf1eb7ecf2f8ebeab5
aggresss <aggresss@163.com>
2021-02-21 21:01:04 +0800

    2021-02-21 21:01:04 CST W08D0

--- workbench/sub_reference.pl
+++ workbench/sub_reference.pl
@@ -4,11 +4,22 @@ use strict;
 
 
 sub func1 ($$) {
-	my ($i, $j)=@_;
-	print "$i\n";
-	print "@$j\n";
+    my ($i, $j)=@_;
+    print "$i\n";
+    print "@$j\n";
 }
 
-our $admin = "foo";
-our @user = ('a', 'b', 'c');
-func1($admin, \@user);
+our $admin1 = "foo";
+our @user1 = ('a', 'b', 'c');
+func1($admin1, \@user1);
+
+
+sub func2(\@\@) {
+    my ($i, $j)=@_;
+    print "@$i\n";
+    print "@$j\n";
+}
+
+our @admin2 = ("foo", "bar", "zoo");
+our @user2 = ('x', 'y', 'z');
+func2(@admin2, @user2);
